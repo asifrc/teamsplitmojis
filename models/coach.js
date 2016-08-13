f6f72a72bcf44f885851c470fcb3df75af718eae
Asif R Choudhury <choudhury.asif@gmail.com>
2016-08-14 02:21:46 +0530

    Extract reusable code from Coach model to generic DiskDBModel

--- models/coach.js
+++ models/coach.js
@@ -1,32 +1,6 @@
 var CoachModel = function(db) {
-  var self = this;
-
-  db.loadCollections(['coaches']);
-
-  var create = function(name) {
-    var coach = db.coaches.save({
-      "Name": name
-    });
-    return coach
-  };
-  self.create = create;
-
-  var findById = function(id) {
-    var coach = db.coaches.findOne({_id: id});
-    return coach;
-  }
-  self.findById = findById;
-
-  var findAll = function() {
-    return db.coaches.find();
-  };
-  self.findAll = findAll;
-
-  var removeAll = function() {
-    db.coaches.remove();
-    db.loadCollections(['coaches']);
-  };
-  self.removeAll = removeAll;
+  var collection = "coaches";
+  var self = require('./diskdbmodel')(collection, db);
 
   self.fields = [
     'Name'
---
+++ models/diskdbmodel.js
@@ -0,0 +1,34 @@
+var DiskDBModel = function(collection, db) {
+
+  var self = this;
+
+  db.loadCollections([collection]);
+
+  var create = function(name) {
+    var coach = db[collection].save({
+      "Name": name
+    });
+    return coach;
+  };
+  self.create = create;
+
+  var findById = function(id) {
+    var coach = db[collection].findOne({_id: id});
+    return coach;
+  }
+  self.findById = findById;
+
+  var findAll = function() {
+    return db[collection].find();
+  };
+  self.findAll = findAll;
+
+  var removeAll = function() {
+    db[collection].remove();
+    db.loadCollections([collection]);
+  };
+  self.removeAll = removeAll;
+
+  return self;
+};
+module.exports = DiskDBModel;
